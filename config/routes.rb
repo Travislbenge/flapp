6e649af861ccff8e6ce25a47903e9c56889980ba
Daniel Blake <daniel.blake1@hotmail.co.uk>
2018-05-23 20:17:34 +0200

    Controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'flashcards/index'
+  get 'flashcards/show'
+  get 'flashcards/new'
+  get 'flashcards/create'
+  get 'flashcards/edit'
+  get 'flashcards/update'
+  get 'flashcards/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
